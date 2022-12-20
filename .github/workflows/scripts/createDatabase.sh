ad66a14c78dda64febd66eb8387d4323dd94666f
Will Fleury <willfleury@gmail.com>
2022-12-20 17:12:20 +0100

    #5 remove debugging cmds in script

---
+++ .github/workflows/scripts/createDatabase.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -e
+
+# add bteq to path
+export PATH=$PATH:"/Users/runner/Library/Application Support/teradata/client/17.10/bin/"
+
+cat << EOF > /tmp/pytestdatabase.bteq
+.SET EXITONDELAY ON MAXREQTIME 20
+.logon 127.0.0.1/dbc,dbc
+CREATE DATABASE DBT_TEST
+  AS PERMANENT = 60e6, SPOOL = 120e6;
+.logoff
+EOF
+
+bteq < /tmp/pytestdatabase.bteq
