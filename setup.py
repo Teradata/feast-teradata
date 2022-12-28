8c0a658b9fa40f756dd3f9fb1dd03ba2fa8bbca2
Will Fleury <willfleury@gmail.com>
2022-12-28 20:03:13 +0100

    issue #13 packaging

--- setup.py
+++ setup.py
@@ -3,18 +3,30 @@ from setuptools import find_packages, setup
 with open('requirements.txt') as f:
     required = f.read().splitlines()
 
+with open("README.md") as f:
+    long_description = f.read()
+
 setup(
     name="feast-teradata",
     version="0.1",
-    author="Teradata",
-    long_description=open("README.md").read(),
+    author="Teradata Corporation",
+    author_email="developers@teradata.com",
+    url="https://github.com/Teradata/feast-teradata",
+    long_description=long_description,
     long_description_content_type="text/markdown",
     python_requires=">=3.7",
     packages=find_packages(exclude=('tests',)),
+    package_data={
+        'feast_teradata.cli': [
+            'template/teradata/**/*.*'
+        ],
+    },
     install_requires=required,
     tests_require=['pytest==6.2.4'],
-    license_files=['LICENSE.txt', 'LICENSE-3RD-PARTY.txt'],
+    license_files=['LICENSE', 'LICENSE-3RD-PARTY.txt'],
+
+    # or script?
     entry_points={'console_scripts': [
-        'create_repo = feast_teradata.cli:main',
+        'create_repo=feast_teradata.cli.create_repo:main',
     ]},
 )
